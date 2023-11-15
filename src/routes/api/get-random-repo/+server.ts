236e4dbe9fdd735a82a41bfbdb217155e21b66d4
zonetecde <zonetecde@gmail.com>
2023-11-15 14:40:21 +0100

    + add-repo

--- src/routes/api/get-random-repo/+server.ts
+++ src/routes/api/get-random-repo/+server.ts
@@ -1,10 +1,17 @@
-import { error } from '@sveltejs/kit';
 import Repo from '../../../models/Repo.js';
-import { sequelize } from '../database/db.js';
+import { Op } from '@sequelize/core';
 
 /** @type {import('./$types').RequestHandler} */
-export async function GET({ url }) {
-	await Repo.create({ repoId: 'test', star: 1, topics: 'test' }); // create the model
+export async function GET({ url }: { url: URL }) {
+	const topics = ['topic1', 'topic2'];
+
+	const matchedRepos = await Repo.findAll({
+		where: {
+			topics: {
+				[Op.and]: topics.map((topic) => ({ [Op.like]: `%,${topic},%` })) // le topic est entre deux virgules
+			}
+		}
+	});
 
 	return new Response(String('test'));
 }
